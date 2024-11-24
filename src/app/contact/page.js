90c7645d99650dc73d6cbd8dd37b144d3aaf0aa0
Sarvin Shrivastava <sarvin5124@gmail.com>
2024-11-24 23:25:14 +0530

    first commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main className="w-2/4 m-auto my-14 text-center">
+            <h1>Contact Page</h1>
+            <p>This is the contact page.</p>
+        </main>
+    );
+};
